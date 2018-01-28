697b03b0ec6882711aaf4a0978550c6120de709c
Kushagr Arora <kushagrarora.17@gmail.com>
2018-01-28 16:43:36 +0530

    modal corrected

--- js/script.js
+++ js/script.js
@@ -90,18 +90,18 @@ $(document).ready(function() {
 
     var span = document.getElementsByClassName("close")[0];
 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    // btn.onclick = function() {
+    //     modal.style.display = "block";
+    // }
+    // 
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // }
+    //
+    // window.onclick = function(event) {
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //     }
+    // }
 
 });
